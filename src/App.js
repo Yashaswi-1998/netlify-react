064ca895f96d10685aace9359e7af4c30f993488
Yashaswi Agrawal <yashaswi@imagekit.io>
2024-07-02 01:57:51 +0530

    path changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
-import cat from './images/cat.jpeg'
-import dog from './images/catDogOne.jpeg'
+// import cat from './images/cat.jpeg'
+// import dog from './images/catDogOne.jpeg'
 import './App.css';
 
 function App() {
@@ -8,8 +8,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <img src={cat} alt="Cat" />
-        <img src={dog} alt="Dog" />
+        <img src='../images/cat.jpeg' alt="Cat" />
+        <img src='../images/catDogOne.jpeg' alt="Dog" />
         <img src="https://placebeard.it/200x300" alt="Bird" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
